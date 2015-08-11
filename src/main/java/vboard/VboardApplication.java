0063aaa4694b899a29ce0b21d7bb1add2c4251fb
emmahastings <emmakhastings@gmail.com>
2015-08-11 20:15:24 +0100

    First commit of project

---
+++ src/main/java/vboard/VboardApplication.java
@@ -0,0 +1,12 @@
+package vboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VboardApplication.class, args);
+    }
+}
